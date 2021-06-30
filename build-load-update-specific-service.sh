19ec77fb63f259f00885eba428ff58fc509884ba
Niklas Göckel <niklasgoeckel@gmail.com>
2021-06-30 15:36:03 +0200

    updated script

--- build-load-update-specific-service.sh
+++ build-load-update-specific-service.sh
@@ -5,5 +5,15 @@
 ms=${1:?Please specify the microservice}
 
 docker build ./src/$ms/ -t $ms || docker build ./src/$ms/src/ -t $ms
+if [ $? -ne 0 ]; then
+    echo "docker image could not be build, aborting ..."
+    exit 1
+fi
+
 kind load docker-image $ms
+if [ $? -ne 0 ]; then
+    echo "docker image could not be loaded into kind cluster, aborting ..."
+    exit 1
+fi
+
 kubectl rollout restart -f kubernetes-manifests/$ms.yaml
